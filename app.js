699f040182ad627340dfef2da3a56a5f53141224
Eric <beatjunkieeric@gmail.com>
2020-06-24 16:39:05 -0700

    display 5 latest repos user has worked on

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ searchUser.addEventListener("keyup", (e) => {
       } else {
         //Show Profile
         ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
     });
   } else {
--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class GitHub {
   constructor() {
     this.client_id = "708261981176baf97843";
     this.client_secret = "0e131d1fe655a898b5eb8cf4498958905b5fc93c";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -9,10 +11,16 @@ class GitHub {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
       profile,
+      repos,
     };
   }
 }
